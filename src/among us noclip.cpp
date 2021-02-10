0cfd2ccc8c423dc8de9e7f6f8749434838e39e84
Viliyan Yordanov <42891941+Vili1@users.noreply.github.com>
2021-02-10 17:31:09 +0200

    New reload feature and fixes
    new reload feature 
    updated pointers

--- src/among us noclip.cpp
+++ src/among us noclip.cpp
@@ -31,6 +31,8 @@ DWORD dwGetModuleBaseAddress(TCHAR* lpszModuleName, DWORD pID)
 
 int main()
 {
+    reload:
+    system("CLS");
     HWND hGameWindow = FindWindow(NULL, "Among Us");
     if (hGameWindow != NULL)
     {
@@ -52,10 +54,10 @@ int main()
         return 0;
     }
 
-    char moduleName[] = "UnityPlayer.dll";
+    char moduleName[] = "GameAssembly.dll";
     DWORD gameBaseAddress = dwGetModuleBaseAddress(_T(moduleName), pID);
-    DWORD offsetGameToBaseAddress = 0x012A7B34;
-    std::vector<DWORD> pointsOffsets{0x8, 0x34, 0x18, 0x18,  0x7C, 0x28, 0x60};
+    DWORD offsetGameToBaseAddress = 0x01C57F7C;
+    std::vector<DWORD> pointsOffsets{0x5C, 0x0, 0x5C, 0x2C, 0x8, 0x5C, 0x2C};
     DWORD baseAddress;
 
     ReadProcessMemory(processHandle, (LPVOID)(gameBaseAddress + offsetGameToBaseAddress), &baseAddress, sizeof(baseAddress), NULL);
@@ -73,7 +75,7 @@ int main()
 
     //second offsets 
     DWORD pointsAddress2 = baseAddress;
-    std::vector<DWORD> pointsOffsets2{0x8, 0x34, 0x18, 0x18,  0x7C, 0x28, 0x64};
+    std::vector<DWORD> pointsOffsets2{0x5C, 0x0, 0x5C, 0x2C, 0x8, 0x5C, 0x30};
     for (int i = 0; i < pointsOffsets2.size() - 1; i++)
     {
         ReadProcessMemory(processHandle, (LPVOID)(pointsAddress2 + pointsOffsets2.at(i)), &pointsAddress2, sizeof(pointsAddress2), NULL);
@@ -88,6 +90,7 @@ int main()
     std::cout << "H- DOWN" << std::endl;
     std::cout << "G- LEFT" << std::endl;
     std::cout << "J- RIGHT" << std::endl;
+    std::cout << "T- Reloads the cheat" << std::endl;
     std::cout << "------------------------------------------------------" << std::endl;
     float xValue = 0;
     float yValue = 0;
@@ -128,6 +131,10 @@ int main()
             yValue -=0.5;
            
             WriteProcessMemory(processHandle, (LPVOID)(pointsAddress2), &yValue, sizeof(float), 0);
+        }
+        if (GetAsyncKeyState(0x54)) // T reload
+        {
+            goto reload;
         }
 
     }
